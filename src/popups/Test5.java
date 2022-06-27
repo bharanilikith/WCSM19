46fd72de424542e0c40969446b39e193d8147e96
QSP <QSP@QSP>
2022-06-27 09:23:06 +0530

    Fileupload

--- src/popups/Test5.java
+++ src/popups/Test5.java
@@ -5,14 +5,21 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class Test5 {
 
 	public static void main(String[] args) {
-
+//chrome
 ChromeOptions option=new ChromeOptions();
 option.addArguments("--disabale notification");
 WebDriver driver=new ChromeDriver(option);
+
+//firefox
+FirefoxOptions options=new FirefoxOptions();
+options.addArguments("--disabale notification");
+WebDriver driver1=new FirefoxDriver(options);
 		
 		driver.manage().window().maximize();
 		driver.get("https://www.olx.in/");
